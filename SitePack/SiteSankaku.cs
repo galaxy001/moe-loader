bc20a83ce149948b89fa056f8c78c76f7c5ee402
esonice <esonice@gmail.com>
2012-12-28 16:07:10 +0000

    fix sankaku download

--- SitePack/SiteSankaku.cs
+++ SitePack/SiteSankaku.cs
@@ -14,7 +14,7 @@ namespace SitePack
 
         public override string SiteUrl { get { return "http://" + sitePrefix + ".sankakucomplex.com"; } }
         public override string SiteName { get { return sitePrefix + ".sankakucomplex.com"; } }
-        public override string ShortName { get { if (sitePrefix == "chan") return "chans"; else return "idol"; } }
+        public override string ShortName { get { if (sitePrefix == "chan") return "chan.s"; else return "idol.s"; } }
         public override bool IsSupportScore
         {
             get { return false; }
