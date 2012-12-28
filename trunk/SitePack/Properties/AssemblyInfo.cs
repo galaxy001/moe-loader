591b451b8349eaee12ecd3f117af6949696e5d23
esonice@gmail.com <esonice@gmail.com@d69a1d85-34ef-ff30-f3d0-1ac83ae91e3b>
2012-12-28 16:07:10 +0000

    fix sankaku download

--- trunk/SitePack/Properties/AssemblyInfo.cs
+++ trunk/SitePack/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("esonic")]
 [assembly: AssemblyProduct("SitePack")]
-[assembly: AssemblyCopyright("Copyright© 2012 esonic")]
+[assembly: AssemblyCopyright("Copyright© 2013 esonic")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.17")]
-[assembly: AssemblyFileVersion("1.0.0.17")]
+[assembly: AssemblyVersion("1.0.0.18")]
+[assembly: AssemblyFileVersion("1.0.0.18")]
