c669b18b5d2bba1b23eb3e17dd7027b01dbd16c7
esonice@gmail.com <esonice@gmail.com@d69a1d85-34ef-ff30-f3d0-1ac83ae91e3b>
2014-07-02 12:50:03 +0000

    fix yandre

--- trunk/SitePack/SitePixiv.cs
+++ trunk/SitePack/SitePixiv.cs
@@ -132,11 +132,16 @@ namespace SitePack
                 //nodes = nodes[nodes.Count - 1].SelectNodes("li");
             }
             else if (srcType == PixivSrcType.Author)
-                nodes = doc.DocumentNode.SelectSingleNode("//div[@class='display_works linkStyleWorks']").SelectSingleNode("ul").SelectNodes("li");
+            {
+                nodes = doc.DocumentNode.SelectSingleNode("//div[@class='display_works linkStyleWorks ']").SelectSingleNode("ul").SelectNodes("li");
+            }
             //else if (srcType == PixivSrcType.Day || srcType == PixivSrcType.Month || srcType == PixivSrcType.Week) //ranking
-                //nodes = doc.DocumentNode.SelectSingleNode("//section[@class='ranking-items autopagerize_page_element']").SelectNodes("div");
-            else //ranking
+            //nodes = doc.DocumentNode.SelectSingleNode("//section[@class='ranking-items autopagerize_page_element']").SelectNodes("div");
+            else
+            {
+                //ranking
                 nodes = doc.DocumentNode.SelectNodes("//section[@class='ranking-item']");
+            }
 
             if (nodes == null)
             {
